67f9a4cb8b27ff6ebf9945ff4b840ef98c61b312
Ngaicode <Mjombakevin376@gmail.com>
2021-11-30 20:22:54 +0300

    modify last sentence

--- mileage_convertor.py
+++ mileage_convertor.py
@@ -2,4 +2,4 @@ print("how many kilometers did you run today?")#prints this out for the user to 
 kms = input() #the input allows us to take user input then assign it to the kms variable
 miles = float(kms)/1.60934 #the value of kms is converted to a float and then assigned to the variable miles
 miles = round(miles,2) #this function rounds off miles to trwo decimal places
-print(f"okay, you ran {miles} miles") #f format allows us to mix different data types
+print(f"okay, your {kms} km  run  is eqaul to {miles} miles") #f format allows us to mix different data types
