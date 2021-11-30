1c5389afb3afe4034ce5a4671e0610656385a4a6
Ngaicode <Mjombakevin376@gmail.com>
2021-11-30 16:30:04 +0300

    make modifications

--- string_concatenation.py
+++ string_concatenation.py
@@ -1,4 +1,5 @@
 str_one = "your"
 str_two = " face is pretty"
-str_three = str_one +  ""  + str_two
+str_three = str_one +  ""  + str_two#you need to add a space so that the words dont get squeezed up together
 print(str_three)
+
