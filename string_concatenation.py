e5d3ee6304d6c4aed7186966008e02b9bfcaee52
Ngaicode <Mjombakevin376@gmail.com>
2021-11-30 17:03:56 +0300

    concatanating different data types with f formating

--- string_concatenation.py
+++ string_concatenation.py
@@ -1,9 +1,12 @@
-"""str_one = "your"
+str_one = "your"
 str_two = " face is pretty"
 str_three = str_one +  ""  + str_two#you need to add a space so that the words dont get squeezed up together
-print(str_three)"""
+print(str_three)
 
 greeting = "hello "
 name = " mwathi"
 greet_name = greeting +  name
 print(greet_name)
+
+guess = 8 #the f format allows you to concatanate different data types
+print(f"your guess of {guess} is incorrect!")
