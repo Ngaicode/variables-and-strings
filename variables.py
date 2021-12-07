137b5b2e58c0f3cd07b2ba8dbb0662547a10525b
Ngaicode <Mjombakevin376@gmail.com>
2021-12-07 15:10:32 +0300

    add comments on strings

--- variables.py
+++ variables.py
@@ -1,2 +1,2 @@
-one , two , three = 1 , 2 , 3 
+one , two , three = 1 , 2 , 3 #you can also assign variables sequentially and tey shall be aasigned with respect to the order of sata you provided
 print(two)
