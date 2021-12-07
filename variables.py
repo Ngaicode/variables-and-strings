9e0be208d3f54214b9a5362bbd870cb09fcc87fd
Ngaicode <Mjombakevin376@gmail.com>
2021-12-07 15:15:17 +0300

    fix typo

--- variables.py
+++ variables.py
@@ -1,2 +1,2 @@
-one , two , three = 1 , 2 , 3 #you can also assign variables sequentially and tey shall be aasigned with respect to the order of sata you provided
+one , two , three = 1 , 2 , 3 #you can also assign variables sequentially and they shall be aasigned with respect to the order of data you provided.
 print(two)
